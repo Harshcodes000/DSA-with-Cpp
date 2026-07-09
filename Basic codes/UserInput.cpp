7e6b8a43adacb90396ec6582cd78540c228af986
Harshcodes000 <harsh.saini0806@gmail.com>
2026-07-09 19:47:36 +0530

    UserInput

---
+++ Basic codes/UserInput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string Name;
+    cout << "Enter Your Name: " ;
+    cin >> Name;
+    cout << "Hello , Welcome to C++ " << Name ;
+    return 0;
+}
