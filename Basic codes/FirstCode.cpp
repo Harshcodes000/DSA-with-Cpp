225046c4d07b03d67b66f9b793f18b2486f5ccbc
Harshcodes000 <harsh.saini0806@gmail.com>
2026-07-09 20:13:30 +0530

    comment

--- Basic codes/FirstCode.cpp
+++ Basic codes/FirstCode.cpp
@@ -4,6 +4,6 @@ int main(){
     cout <<"Welcome to this C++ series";
     return 0;
 }
-//Compile : g++ -o FirstCode.cpp
-//Run : .\FirstCode
+//Compile : g++ FirstCode.cpp
+//Run : ./FirstCode
 //Output: Welcome to this C++ series
