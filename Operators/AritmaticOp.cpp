d67228719905c117aa8696fe2151c3b820fa998c
Harshcodes000 <harsh.saini0806@gmail.com>
2026-07-10 22:41:32 +0530

    Managed files

--- Operators/AritmaticOp.cpp
+++ Operators/AritmaticOp.cpp
@@ -11,7 +11,8 @@ int main() {
     cout << "a + b = " << (a + b) << "\n";
     cout << "a - b = " << (a - b) << "\n";
     cout << "a * b = " << (a * b) << "\n";
-
+    
+    //Conditional Statement
     if (b != 0) {
         cout << "a / b = " << (a / b) << "\n";
         cout << "a % b = " << (a % b) << "\n";
