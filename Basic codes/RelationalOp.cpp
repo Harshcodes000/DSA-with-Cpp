508f6793e664430f61236641889b662b553e7f64
Harshcodes000 <harsh.saini0806@gmail.com>
2026-07-10 22:12:14 +0530

    Relational Operators

---
+++ Basic codes/RelationalOp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter two integers (a and b): ";
+    cin >> a >> b;
+
+    cout << "\n--- Relational Operators ---\n";
+    cout << "a == b : " << (a == b) << "\n";
+    cout << "a != b : " << (a != b) << "\n";
+    cout << "a <  b : " << (a < b)  << "\n";
+    cout << "a <= b : " << (a <= b) << "\n";
+    cout << "a >  b : " << (a > b)  << "\n";
+    cout << "a >= b : " << (a >= b) << "\n";
+
+    return 0;
+}
+
+
+
